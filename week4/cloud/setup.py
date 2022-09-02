23ead6776af2bc0900fa57f740e585e758b03d00
leon <ishimweleon@gmail.com>
2022-09-03 02:45:50 +0300

    added cloud

---
+++ week4/cloud/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello-cloud-functions',
+
+    version='1.0.0-SNAPSHOT',
+
+    description='',
+    long_description='',
+
+    author='Steve Willis',
+    author_email='some@email.com',
+
+    license='Copyright (c) 2018. All rights reserved.',
+
+    packages=find_packages(),
+    namespace_packages=['hello-cloud-functions'],
+    zip_safe=False,
+)
+
