d99de33f6c7d683a3dcba5bd1bb20c366ed16bfa
leon <ishimweleon@gmail.com>
2022-09-03 03:36:19 +0300

    finished week 5

--- week5/cloud/main.py
+++ week5/cloud/main.py
@@ -1,23 +1,19 @@
 import functions_framework
+import pickle
+
+targets = ["setosa", "versicolor", "virginica"]
+
 
 @functions_framework.http
-def hello_http(request):
-   """HTTP Cloud Function.
-   Args:
-       request (flask.Request): The request object.
-       <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-   Returns:
-       The response text, or any set of values that can be turned into a
-       Response object using `make_response`
-       <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-   """
-   request_json = request.get_json(silent=True)
-   request_args = request.args
+def hello_world(request):
+    model = pickle.load(open("model.pickle", "rb"))
 
-   if request_json and 'name' in request_json:
-       name = request_json['name']
-   elif request_args and 'name' in request_args:
-       name = request_args['name']
-   else:
-       name = 'World'
-   return 'Hello {}!'.format(name)
+    slength = float(request.args.get("slength"))
+    swidth = float(request.args.get("swidth"))
+
+    plength = float(request.args.get("plength"))
+    pwidth = float(request.args.get("pwidth"))
+    
+    res = model.predict([[slength, swidth, plength, pwidth]])
+    return targets[res[0]]
+
